1d85859b82f94d39bb61fc018ca08532c2cef142
Edwin Eefting <edwin@datux.nl>
2016-05-23 13:47:45 +0200

    license stuff

--- zarafa_spamd/__init__.py
+++ zarafa_spamd/__init__.py
@@ -16,6 +16,23 @@ sys.path.insert(0, os.path.dirname(__file__)) # XXX for __import__ to work
 """
 zarafa-spamd v1 - monitors mail-movements to retrain your spamfilter
 
+Created by Edwin Eefting (edwin@datux.nl), based on zarafa-search and the excellent python-zarafa API.
+
+Copyright 2005 - 2015  Zarafa B.V. and its licensors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License, version 3,
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+
+
 """
 
 CONFIG = {
